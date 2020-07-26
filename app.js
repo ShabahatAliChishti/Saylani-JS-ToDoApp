3637ab8c9593fd8835fdfcdfe9ae67b1d4c40d4f
shabahat97 <shabahatalichishti@gmail.com>
2020-07-26 18:32:15 +0500

    ToDoApp

---
+++ app.js
@@ -0,0 +1,51 @@
+var myTodoList = document.getElementById("todoList");
+
+function AddTodoItem() {
+    var todoItem = document.getElementById("todoItem");
+    var todoItemValue = todoItem.value;
+    if(todoItemValue != "") {
+        var li = document.createElement("li");
+        var todoItemText = document.createTextNode(todoItemValue);
+        var editButton = document.createElement("button");
+        var deleteButton = document.createElement("button");
+
+        todoItem.value = "";
+        editButton.innerText = "Edit";
+        editButton.className = "editBtn";
+        editButton.setAttribute("onclick","editTodoItem(this)");
+        deleteButton.innerText = "Delete";
+        deleteButton.className = "deleteBtn";
+        deleteButton.setAttribute("onclick","deleteTodoItem(this)");
+       
+        
+        li.appendChild(todoItemText);
+        myTodoList.appendChild(li);
+        li.appendChild(editButton);
+        li.appendChild(deleteButton);
+    }
+    else {
+        alert("Please enter some value to Add!");
+    }
+}
+
+function deleteTodoItem(event) {
+    event.parentNode.remove(this);
+}
+
+function deleteAllTodoItems() {
+    if(myTodoList.firstChild!=null)
+    {
+    myTodoList.innerHTML = "";
+    }
+    else {
+        alert("There is no item in your list!");
+    }
+    
+}
+
+function editTodoItem(e)
+{
+    var editValue = prompt("Enter Updated Value ", e.parentNode.firstChild.nodeValue);
+    e.parentNode.firstChild.nodeValue =  editValue;
+    
+}
